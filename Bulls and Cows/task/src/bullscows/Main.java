984ab1603419e3a01c8c69ab83c1ad912c3234f6
Max Pestov <pestov.maxik@gmail.com>
2024-02-05 20:54:20 +0400

    The third stage completed

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -1,12 +1,36 @@
 package bullscows;
 
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String secret = Integer.toString(
+
+        int size = scanner.nextInt();
+        if (size > 10) {
+            System.out.println("Error: can't generate a secret number with a length of 11 " +
+                    "because there aren't enough unique digits.\n");
+            return;
+        }
+        StringBuilder randomNumberBuilder = new StringBuilder();
+        Set<Integer> alphabet = new HashSet<>();
+        while (randomNumberBuilder.length() < size) {
+            char[] nanos = Long.toString(System.nanoTime()).toCharArray();
+            for (int i = nanos.length - 1; i >= 0 && randomNumberBuilder.length() < size; i--) {
+                int number = Character.getNumericValue(nanos[i]);
+                if (alphabet.contains(number) || number == 0 && randomNumberBuilder.isEmpty()) {
+                    continue;
+                }
+                alphabet.add(number);
+                randomNumberBuilder.append(number);
+            }
+        }
+        System.out.printf("The random secret number is %s.%n", randomNumberBuilder);
+
+        /*String secret = Integer.toString(
                 new Random().nextInt(10000 - 1000) + 1000);
         Grader grader = new GuessGrader(secret.toCharArray());
 
@@ -16,7 +40,7 @@ public class Main {
         System.out.printf(
                 "Grade: %s. The secret code is %s.%n",
                 gradeMessage,
-                String.join("", secret));
+                String.join("", secret));*/
     }
 
     private static String getGradeMessage(Grade grade) {
